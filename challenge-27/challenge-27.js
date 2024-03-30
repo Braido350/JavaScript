b79d9c54f7e2ac9a2cc0a2880fe1e2d782017e3b
Lenovo <braido350@gmail.com>
2024-03-30 15:23:21 -0400

    simplificado o codigo

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -22,86 +22,65 @@
   O método isNull deve retornar `true` se o valor for null ou undefined.
   */
 
-  function DOM(elements){
-    this.element = document.querySelectorAll(elements);
+  class DOM {
+    constructor(elements) {
+      this.element = document.querySelectorAll(elements);
+    }
+    on(eventType, callback) {
+      Array.prototype.forEach.call(this.element, function (element) {
+        element.addEventListener(eventType, callback, false);
+      });
+    }
+    off(eventType, callback) {
+      Array.prototype.forEach.call(this.element, function (element) {
+        element.removeEventListener(eventType, callback, false);
+      });
+    }
+    get() {
+      return this.element;
+    }
+    forEach() {
+      return Array.prototype.forEach.apply(this.element, arguments);
+    }
+    map() {
+      return Array.prototype.map.apply(this.element, arguments);
+    }
+    filter() {
+      return Array.prototype.filter.apply(this.element, arguments);
+    }
+    reduce() {
+      return Array.prototype.reduce.apply(this.element, arguments);
+    }
+    reduceRight() {
+      return Array.prototyp.reduceRight.apply(this.element, arguments);
+    }
+    every() {
+      return Array.prototype.every.apply(this.element, arguments);
+    }
+    some() {
+      return Array.prototype.some.apply(this.element, arguments);
+    }
+    isArray(obj) {
+      return Object.prototype.toString.call(obj) === '[object Array]';
+    }
+    isObject(obj) {
+      return Object.prototype.toString.call(obj) === '[object Object]';
+    }
+    isFunction(obj) {
+      return Object.prototype.toString.call(obj) === '[object Function]';
+    }
+    isNumber(obj) {
+      return Object.prototype.toString.call(obj) === '[object Number]';
+    }
+    isString(obj) {
+      return Object.prototype.toString.call(obj) === '[object String]';
+    }
+    isBoolean(obj) {
+      return Object.prototype.toString.call(obj) === '[object Boolean]';
+    }
+    isNull(obj) {
+      return Object.prototype.toString.call(obj) === '[object Null]' || Object.prototype.toString.call(obj) === '[object Undefined]';
+    }
   };
-
-  DOM.prototype.on = function on(eventType, callback) {
-    Array.prototype.forEach.call(this.element, function (element) {
-      element.addEventListener(eventType, callback, false);
-    });
-  };
-
-  DOM.prototype.off = function off(eventType, callback) {
-    Array.prototype.forEach.call(this.element, function (element) {
-      element.removeEventListener(eventType, callback, false);
-    });
-  };
-
-  DOM.prototype.get = function get(){
-    return this.element;
-  };
-
-  DOM.prototype.forEach = function forEach(){
-    return Array.prototype.forEach.apply(this.element, arguments)
-  };
-
-  DOM.prototype.map = function map(){
-    return Array.prototype.map.apply(this.element, arguments)
-  };
-
-  DOM.prototype.filter = function filter(){
-    return Array.prototype.filter.apply(this.element, arguments)
-  };
-
-  DOM.prototype.reduce = function reduce(){
-    return Array.prototype.reduce.apply(this.element, arguments)
-  };
-
-  DOM.prototype.reduceRight = function reduceRight(){
-    return Array.prototyp.reduceRight.apply(this.element, arguments)
-  };
-
-  DOM.prototype.every = function every(){
-    return Array.prototype.every.apply(this.element, arguments)
-  };
-
-  DOM.prototype.some = function some(){
-    return Array.prototype.some.apply(this.element, arguments)
-  };
-
-  DOM.prototype.isArray = function isArray(obj){
-    return Object.prototype.toString.call(obj) === '[object Array]'
-  }
-
-  DOM.prototype.isObject = function isObject(obj){
-    return Object.prototype.toString.call(obj) === '[object Object]'
-  }
-
-  DOM.prototype.isFunction = function isFunction(obj){
-    return Object.prototype.toString.call(obj) === '[object Function]'
-  }
-
-  DOM.prototype.isNumber = function isNumber(obj){
-    return Object.prototype.toString.call(obj) === '[object Number]'
-  }
-
-  DOM.prototype.isString = function isString(obj){
-    return Object.prototype.toString.call(obj) === '[object String]'
-  }
-
-  DOM.prototype.isBoolean = function isBoolean(obj){
-    return Object.prototype.toString.call(obj) === '[object Boolean]'
-  }
-
-  DOM.prototype.isNull = function isNull(obj){
-    return Object.prototype.toString.call(obj) === '[object Null]' || Object.prototype.toString.call(obj) === '[object Undefined]'
-  };
-
-  // forEach, map, filter, reduce, reduceRight, every e some.
-  // isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-
-  var dom = new DOM
-  console.log(dom.isNull())
 
 }(window, document));
